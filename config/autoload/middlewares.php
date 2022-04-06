92b0d9f6d64ea4544921c76e79e3bc908482e3fa
yangwb1123 <1832227346@qq.com>
2022-04-06 08:22:00 +0800

    feat(middleware): add cors middleware.

--- config/autoload/middlewares.php
+++ config/autoload/middlewares.php
@@ -2,8 +2,11 @@
 
 declare(strict_types=1);
 
+use App\Middleware\CorsMiddleware;
+
 return [
     'http' => [
-        \Hyperf\Validation\Middleware\ValidationMiddleware::class
+        \Hyperf\Validation\Middleware\ValidationMiddleware::class,
+        App\Middleware\CorsMiddleware::class,
     ],
 ];
