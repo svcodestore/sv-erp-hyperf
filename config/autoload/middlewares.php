13896069ba08991d250171ef588fa69d4cd860de
mokch <buxueys@gmail.com>
2022-05-04 10:09:12 +0800

    0504

--- config/autoload/middlewares.php
+++ config/autoload/middlewares.php
@@ -4,8 +4,8 @@ declare(strict_types=1);
 
 return [
     'http' => [
-        \Hyperf\Validation\Middleware\ValidationMiddleware::class,
-        App\Middleware\CorsMiddleware::class,
-        App\Middleware\SsoMiddleware::class,
+        // \Hyperf\Validation\Middleware\ValidationMiddleware::class,
+        // App\Middleware\CorsMiddleware::class,
+        // App\Middleware\SsoMiddleware::class,
     ],
 ];
--- config/routes.php
+++ config/routes.php
@@ -5,12 +5,18 @@ declare(strict_types=1);
 use App\Controller\AuthController;
 use App\Controller\OAuthController;
 use App\Controller\Hr\KPI\ItemCategoryController;
+use Hyperf\HttpServer\Router\Router;
 use App\Controller\Hr\KPI\ItemController;
 use App\Controller\Hr\KPI\RankController;
+use App\Controller\Hr\KPI\TitleController;
 use App\Controller\Prod\ScheduleController;
 use App\Controller\Application\ApplicationController;
 use App\Controller\Bs\OrderController;
 use Hyperf\HttpServer\Router\Router;
+use App\Controller\Hr\KPI\PositionController;
+use App\Controller\Hr\KPI\ItemCategoryController;
+use App\Controller\Hr\KPI\PositionGroupController;
+use App\Controller\Hr\KPI\TitleCategoryController;
 
 Router::addRoute(['GET', 'POST', 'HEAD'], '/', 'App\Controller\IndexController@sayHello');
 Router::addRoute(['GET'], '/api', 'App\Controller\IndexController@index');
@@ -69,5 +75,8 @@ Router::addGroup('/api', function () {
 Router::addServer('grpc', function () {
     Router::addGroup('/grpc.hi', function () {
         Router::post('/sayHello', 'App\Controller\IndexController@sayHello');
+        Router::addGroup('/tpm', function () {
+            Router::addRoute(['GET'], '/fittings', [\App\Controller\TPM\FittingController::class, 'fittings']);
+        });
     });
 });
