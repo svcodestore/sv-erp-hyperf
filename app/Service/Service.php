d2aeb563930d976aaeaac1ae0ac8157e22b49acd
yangwb1123 <1832227346@qq.com>
2022-06-16 17:02:32 +0800

    feat(service): add `crudBatch`

--- app/Service/Service.php
+++ app/Service/Service.php
@@ -3,6 +3,9 @@
 namespace App\Service;
 
 use App\Model\Model;
+use App\Util\StrUtil;
+use Hyperf\DbConnection\Db;
+use Hyperf\HttpServer\Contract\RequestInterface;
 
 class Service implements IService
 {
@@ -12,5 +15,34 @@ class Service implements IService
         $statement = $m::query();
         if (empty($cond)) return $statement->get($cols)->toArray();
         return $statement->where($cond)->get($cols)->toArray();
+    }
+
+    public function crudBatch(Model $m, RequestInterface $request): bool
+    {
+        $create = StrUtil::withSnakeCase($request->input('A', []));
+        $delete = StrUtil::withSnakeCase($request->input('D', []));
+        $update = StrUtil::withSnakeCase($request->input('U', []));
+        if (empty($create) && empty($delete) && empty($update)) return true;
+
+        Db::beginTransaction();
+
+        if (!empty($delete)) {
+            $m::destroy($delete);
+        }
+
+        if (!empty($update)) {
+            foreach ($update as $updateItem) {
+                $m::query()->where('id', $updateItem['id'])->update($updateItem);
+            }
+        }
+
+        if (!empty($create)) {
+            foreach ($create as $createItem) {
+                foreach (array_keys($createItem) as $key) {
+                    $m->$key = $createItem[$key];
+                }
+                $m->save();
+            }
+        }
     }
 }
