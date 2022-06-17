aeccc3a2baf2a89320208164baebfb7e10c3a912
yangwb1123 <1832227346@qq.com>
2022-06-17 17:04:39 +0800

    feat(util): `ArrayUtil` add array to tree

--- app/Util/ArrayUtil.php
+++ app/Util/ArrayUtil.php
@@ -30,4 +30,70 @@ class ArrayUtil
 
         return $new;
     }
+
+    // public static function toTree(array $arr, string $idKey = 'id', string $pidKey = 'pid', $childrenKey = 'children'): array
+    // {
+    //     $tree = [];
+    //     if (empty($arr)) return $tree;
+
+    //     // 父主键数组
+    //     $pids = array_map(fn ($e) => $e[$pidKey], $arr);
+
+    //     // 主键映射
+    //     $idMapRefer = [];
+    //     foreach ($arr as $key => $data) {
+    //         $idMapRefer[$data[$idKey]] = &$arr[$key];
+    //     }
+
+    //     // 找父结点
+    //     foreach ($arr as $key => $data) {
+    //         if (in_array($data[$idKey], $pids)) {
+    //             unset($pidKeys[$data[$pidKey]]);
+    //         }
+    //     }
+
+    //     foreach ($arr as $key => $data) {
+    //         $pid = $data[$pidKey];
+    //         if (in_array($pid, $pids)) {
+    //             $tree[] = &$arr[$key];
+    //         } else {
+    //             $parent = &$idMapRefer[$pid];
+    //             $parent[$childrenKey][] = &$arr[$key];
+    //         }
+    //     }
+
+    //     return $tree;
+    // }
+
+    public static function toTree(array $list, string $pk = 'id', string $pid = 'pid', $child = 'children'): array
+    {
+        $tree = array();
+        if (!is_array($list)) {
+            return $tree;
+        }
+        $refer = array();
+        $parentNodeIdArr = [];
+        foreach ($list as $key => $data) {
+            $refer[$data[$pk]] = &$list[$key];
+            $parentNodeIdArr[$data[$pid]] = $data[$pid];
+        }
+        /* 寻找根结点 */
+        foreach ($list as $key => $data) {
+            if (in_array($data[$pk], $parentNodeIdArr)) {
+                unset($parentNodeIdArr[$data[$pk]]);
+            }
+        }
+        foreach ($list as $key => $data) {
+            $parantId = $data[$pid];
+            if (in_array($parantId, $parentNodeIdArr)) {
+                $tree[] = &$list[$key];
+            } else {
+                if (isset($refer[$parantId])) {
+                    $parent = &$refer[$parantId];
+                    $parent[$child][] = &$list[$key];
+                }
+            }
+        }
+        return $tree;
+    }
 }
