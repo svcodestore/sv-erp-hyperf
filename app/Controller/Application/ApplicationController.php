c46fd64e4b68192cbd6d133ff595e6e28d91f51c
yangwb1123 <1832227346@qq.com>
2022-06-30 12:20:42 +0800

    fix: 应用信息多回调地址处理

--- app/Controller/Application/ApplicationController.php
+++ app/Controller/Application/ApplicationController.php
@@ -8,7 +8,7 @@ use App\Controller\AbstractController;
 use App\Service\ApplicationService;
 use Hyperf\Di\Annotation\Inject;
 
-class ApplicationController  extends AbstractController
+class ApplicationController extends AbstractController
 {
     /**
      * @Inject
@@ -18,7 +18,30 @@ class ApplicationController  extends AbstractController
 
     public function getCurrentApplication()
     {
-        $data = $this->applicationService->currentApplication();
-        return $this->responseOk($data);
+        $application = $this->applicationService->currentApplication();
+
+        $isIntranet = true;
+        foreach (explode(".", $this->request->getServerParams()['remote_addr']) as $segment) {
+            $isIntranet = $isIntranet && is_numeric($segment);
+        }
+
+        $redirectUris = explode("|", $application["redirectUris"]);
+        if (count($redirectUris) > 1) {
+            if ($isIntranet) {
+                $application["redirectUris"] = $redirectUris[0];
+            } else {
+                $application["redirectUris"] = $redirectUris[1];
+            }
+        }
+        $loginUris = explode("|", $application["loginUris"]);
+        if (count($loginUris) > 1) {
+            if ($isIntranet) {
+                $application["loginUris"] = $loginUris[0];
+            } else {
+                $application["loginUris"] = $loginUris[1];
+            }
+        }
+
+        return $this->responseOk($application);
     }
 }
