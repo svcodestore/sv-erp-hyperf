76be4c892f34d5226e291d9dae611fb4c2d6d578
yangwb1123 <1832227346@qq.com>
2022-07-19 13:34:36 +0800

    refactor(middleware): allow * methods for cors

--- config/autoload/middlewares.php
+++ config/autoload/middlewares.php
@@ -2,10 +2,14 @@
 
 declare(strict_types=1);
 
+use App\Middleware\CorsMiddleware;
+use App\Middleware\SsoMiddleware;
+use Hyperf\Validation\Middleware\ValidationMiddleware;
+
 return [
     'http' => [
-        \Hyperf\Validation\Middleware\ValidationMiddleware::class,
-        App\Middleware\CorsMiddleware::class,
-        App\Middleware\SsoMiddleware::class,
+        ValidationMiddleware::class,
+        CorsMiddleware::class,
+        SsoMiddleware::class,
     ],
 ];
