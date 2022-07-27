191fef76c256ccd3d41b128e16ce38db8308be05
yangwb1123 <1832227346@qq.com>
2022-07-27 14:25:27 +0800

    feat(sql): 添加 hr_mpr 相关的表

--- sv_erp.sql
+++ sv_erp.sql
@@ -333,3 +333,137 @@ create table hr_kpi_staff_score
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   COLLATE = utf8mb4_unicode_ci;
+
+create table hr_mpr_currency
+(
+    `id`   int auto_increment,
+    `code` varchar(8) not null,
+    `name` varchar(16),
+    primary key (`id`),
+    index hr_mpr_currency_index_code (`code`),
+    constraint unique hr_mpr_currency_unique_index (`code`)
+) engine = InnoDB
+  default charset = utf8mb4
+  collate = utf8mb4_unicode_ci;
+
+create table hr_mpr_currency_exchange_rate
+(
+    `id`               int auto_increment,
+    `currency_code`    varchar(8)             not null,
+    `to_currency_code` varchar(8)             not null,
+    `rate`             decimal(6, 3) unsigned not null,
+    primary key (`id`),
+    index hr_mpr_currency_exchange_rate_index_currency (`currency_code`),
+    index hr_mpr_currency_exchange_rate_index_to_currency (`to_currency_code`),
+    constraint unique hr_mpr_currency_exchange_rate_unique_index (`currency_code`, `to_currency_code`),
+    constraint hr_mpr_currency_exchange_rate_fk_currency_code foreign key (`currency_code`) references hr_mpr_currency (`code`) on update cascade on delete cascade,
+    constraint hr_mpr_currency_exchange_rate_fk_to_currency_code foreign key (`to_currency_code`) references hr_mpr_currency (`code`) on update cascade on delete cascade
+) engine = InnoDB
+  default charset = utf8mb4
+  collate = utf8mb4_unicode_ci;
+
+create table hr_mpr_group
+(
+    `id`   int auto_increment,
+    `pid`  int        not null,
+    `name` varchar(8) not null,
+    primary key (`id`),
+    constraint unique hr_mpr_group_unique_index (`pid`, `name`)
+) engine = InnoDB
+  default charset = utf8mb4
+  collate = utf8mb4_unicode_ci;
+
+create table hr_mpr_human
+(
+    `id`                 int auto_increment,
+    `year`               smallint unsigned not null,
+    `month`              tinyint unsigned  not null,
+    `region`             tinyint unsigned  not null comment '0 => all; 1 => SV; 2 => JS',
+    `category`           tinyint unsigned  not null comment '0 => all; 1 => indirect; 2 => direct',
+    `estimated_manpower` smallint unsigned not null,
+    `actual_manpower`    smallint unsigned not null,
+    `created_at`         datetime(6)       not null default current_timestamp(6),
+    `created_by`         bigint            not null,
+    `updated_at`         datetime(6)       not null default current_timestamp(6) on update current_timestamp(6),
+    `updated_by`         bigint            not null,
+    primary key (`id`),
+    index hr_mpr_human_index_year (`year`),
+    index hr_mpr_human_index_region (`region`),
+    index hr_mpr_human_index_category (`category`),
+    constraint unique hr_mpr_human_unique_index (`year`, `month`, `region`, `category`)
+) engine = InnoDB
+  default charset = utf8mb4
+  collate = utf8mb4_unicode_ci;
+
+create table hr_mpr_working_hours
+(
+    `id`              int auto_increment,
+    `year`            smallint unsigned not null,
+    `month`           tinyint unsigned  not null,
+    `region`          tinyint unsigned  not null comment '0 => all; 1 => SV; 2 => JS',
+    `category`        tinyint unsigned  not null comment '0 => all; 1 => indirect; 2 => direct',
+    `sub_category`    tinyint unsigned  not null comment '0 => all; 1 => month; 2 => hour',
+    `estimated_hours` int unsigned      not null,
+    `actual_hours`    int unsigned      not null,
+    `created_at`      datetime(6)       not null default current_timestamp(6),
+    `created_by`      bigint            not null,
+    `updated_at`      datetime(6)       not null default current_timestamp(6) on update current_timestamp(6),
+    `updated_by`      bigint            not null,
+    primary key (`id`),
+    index hr_mpr_working_hours_index_year (`year`),
+    index hr_mpr_working_hours_index_region (`region`),
+    index hr_mpr_working_hours_index_category (`category`),
+    constraint unique hr_mpr_working_hours_unique_index (`year`, `month`, `region`, `category`, `sub_category`)
+) engine = InnoDB
+  default charset = utf8mb4
+  collate = utf8mb4_unicode_ci;
+
+create table hr_mpr_stock_out_in
+(
+    `id`                           int auto_increment,
+    `year`                         smallint unsigned not null,
+    `month`                        tinyint unsigned  not null,
+    `region`                       tinyint unsigned  not null comment '0 => all; 1 => SV; 2 => JS',
+    `category`                     tinyint unsigned  not null comment '0 => all; 1 => indirect; 2 => direct',
+    `currency_id`                  int               not null,
+    `currency`                     varchar(16)       not null,
+    `estimated_shipment_money`     bigint unsigned   not null,
+    `actual_shipment_money`        bigint unsigned   not null,
+    `shipment_achievement_rate`    int,
+    `estimated_warehousing_money`  bigint unsigned   not null,
+    `actual_warehousing_money`     bigint unsigned   not null,
+    `warehousing_achievement_rate` int,
+    `created_at`                   datetime(6)       not null default current_timestamp(6),
+    `created_by`                   bigint            not null,
+    `updated_at`                   datetime(6)       not null default current_timestamp(6) on update current_timestamp(6),
+    `updated_by`                   bigint            not null,
+    primary key (`id`),
+    index hr_mpr_human_index_year (`year`),
+    index hr_mpr_human_index_region (`region`),
+    index hr_mpr_human_index_category (`category`),
+    constraint unique hr_mpr_human_unique_index (`year`, `month`, `region`, `category`)
+) engine = InnoDB
+  default charset = utf8mb4
+  collate = utf8mb4_unicode_ci;
+
+create table hr_mpr_estimated
+(
+    `id`            int auto_increment,
+    `year`          smallint unsigned not null,
+    `month`         tinyint unsigned  not null,
+    `manpower`      int unsigned,
+    `money`         int unsigned,
+    `working_hours` int unsigned,
+    `group_id`      int               not null,
+    `group_name`    varchar(64),
+    `created_at`    datetime(6)       not null default current_timestamp(6),
+    `created_by`    bigint            not null,
+    `updated_at`    datetime(6)       not null default current_timestamp(6) on update current_timestamp(6),
+    `updated_by`    bigint            not null,
+    primary key (`id`),
+    index hr_mpr_estimated_index_year (`year`),
+    constraint unique hr_mpr_estimated_unique_index (`year`, `month`, `group_id`),
+    constraint hr_mpr_estimated_fk_group_id foreign key (`group_id`) references hr_mpr_group (`id`) on update cascade
+) engine = InnoDB
+  default charset = utf8mb4
+  collate = utf8mb4_unicode_ci;
